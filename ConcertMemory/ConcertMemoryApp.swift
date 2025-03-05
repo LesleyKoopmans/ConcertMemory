cf3886a8fe392f21bd100d5076863218fbc58d2e
Lesley Koopmans <lesleykoopmans@gmail.com>
2025-03-05 13:43:55 +0100

    Initial Commit

---
+++ ConcertMemory/ConcertMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertMemoryApp.swift
+//  ConcertMemory
+//
+//  Created by Lesley Koopmans on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConcertMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcertMemoryTests/ConcertMemoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertMemoryTests.swift
+//  ConcertMemoryTests
+//
+//  Created by Lesley Koopmans on 05/03/2025.
+//
+
+import Testing
+@testable import ConcertMemory
+
+struct ConcertMemoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
