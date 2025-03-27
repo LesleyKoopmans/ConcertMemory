7b78cd6de1cbae9fc28fae5d02120272c286d70f
Lesley Koopmans <lesleykoopmans@gmail.com>
2025-03-27 14:32:55 +0100

    [Feature] Sign in anonymously

--- ConcertMemory/Core/AppView/AppView.swift
+++ ConcertMemory/Core/AppView/AppView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AppView: View {
     
+    @Environment(\.authService) private var authService
     @State var appState = AppState()
     
     var body: some View {
@@ -22,6 +23,23 @@ struct AppView: View {
             }
         )
         .environment(appState)
+        .task {
+            await checkUserStatus()
+        }
+    }
+    
+    private func checkUserStatus() async {
+        if let user = authService.getAuthenticatedUser() {
+            print("User already authenticated: \(user.uid)")
+        } else {
+            do {
+                let result = try await authService.signInAnomymously()
+                
+                print("Sign in anonymous succes: \(result.user.uid)")
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
--- ConcertMemory/Core/ConcertMemoryApp.swift
+++ ConcertMemory/Core/ConcertMemoryApp.swift
@@ -11,7 +11,7 @@ import Firebase
 @main
 struct ConcertMemoryApp: App {
     
-    @UIApplicationDelegateAdaptor(AppDelegate.self)
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
